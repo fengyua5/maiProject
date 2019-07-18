b5e2072cffb2b41d1074ce1a40425197bebf4670
fengyua5 <liziyuanyuzhou@163.com>
2019-07-19 00:20:36 +0800

    add antd

--- config-overrides.js
+++ config-overrides.js
@@ -3,12 +3,18 @@
 const {
     override,
     addLessLoader,
+    fixBabelImports
 } = require("customize-cra");
 
 
 module.exports = override(
+    fixBabelImports("import", {
+        libraryName: "antd", libraryDirectory: "es", style: true // change importing css to less
+    }),
     addLessLoader({
         javascriptEnabled: true,
-        modifyVars: { "@primary-color": "#1DA57A" }
+        modifyVars: {
+            // "@primary-color": "#1DA57A"
+        }
     })
 );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,10 +4,15 @@
 
 import React from 'react';
 
+import {Button} from 'antd';
+
 import styles from './index.module.less'
 
 export default function Home(props) {
     return (
-        <div className={styles.container}>home</div>
+        <div className={styles.container}>
+            home
+            <Button>button</Button>
+        </div>
     )
 }
